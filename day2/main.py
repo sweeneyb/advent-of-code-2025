b8cfd73321263e3c5da18eab07474cc84e5baef6
sweeneyb <tab126@gmail.com>
2025-12-07 13:29:50 -0500

    refactor to use a generator

--- day2/main.py
+++ day2/main.py
@@ -10,18 +10,24 @@ def is_valid(id: str) -> bool:
         return False
     return True
 
-def part_one():
-    invalid_accumulator = 0
-    # with open('input/example.txt', "r") as file:
-    with open('input/input.txt', "r") as file:
+def generate_next_id(file: str) -> str:
+     with open('input/input.txt', "r") as file:
         for line in file:
             text_range = [x.strip() for x in line.split(",")]
             for r in text_range:
                 begin, end = r.split("-")
                 print(f"Range: {begin} to {end}")
                 for i in range (int(begin), int(end)+1):
-                    if not is_valid(str(i)):
-                        invalid_accumulator += i
+                    yield i
+    
+
+def part_one():
+    invalid_accumulator = 0
+    # scanner = generate_next_id('input/example.txt')
+    scanner = generate_next_id('input/input.txt')
+    for i in scanner:
+        if not is_valid(str(i)):
+            invalid_accumulator += i
     print (invalid_accumulator)
 
 
